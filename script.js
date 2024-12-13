94fcfd20aa31ae41d6170aee7810f54a556d4a88
Savchuk Volodymyr <131377510+svstm@users.noreply.github.com>
2024-12-13 14:48:37 +0100

    1.0.1

--- script.js
+++ script.js
@@ -12,3 +12,42 @@ var swiper = new Swiper(".list_clients_swiper", {
   },
   loop: true,
 });
+
+// var swiper2 = new Swiper(".mySwiper", {
+//   grabCursor: true,
+//   effect: "creative",
+//   creativeEffect: {
+//     prev: {
+//       shadow: true,
+//       translate: [0, 0, -400],
+//     },
+//     next: {
+//       translate: ["100%", 0, 0],
+//     },
+//   },
+// });
+
+var swiper2 = new Swiper(".mySwiper", {
+  // thumbs: {
+  //   swiper: text_reviews, // Синхронизация с миниатюрами
+  // },
+});
+
+var text_reviews = new Swiper(".text_reviews", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  thumbs: {
+    swiper: swiper2, // Синхронизация с миниатюрами
+  },
+});
+
+AOS.init();
